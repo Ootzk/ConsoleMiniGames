1579153bae3f271df40dbf0a1093a8f654c93b71
Namho Kwon <blitz555@dgist.ac.kr>
2021-09-02 17:43:54 +0900

    rollback main.cpp

--- ConsoleMiniGames/Main/main.cpp
+++ ConsoleMiniGames/Main/main.cpp
@@ -1,14 +1,8 @@
 #include "engine.h"
 
 int main() {
-	std::ifstream textfile("text.txt", std::fstream::in);
-	std::ifstream fontfile("font.txt", std::fstream::in);
-	std::ifstream backfile("back.txt", std::fstream::in);
-
-	draw(textfile, fontfile, backfile);
-
-	//Engine e;
-	//e.run();
+	Engine e;
+	e.run();
 
 	return 0;
 }
